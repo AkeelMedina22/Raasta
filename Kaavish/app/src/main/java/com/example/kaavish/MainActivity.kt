b04c14a52e9d1e3312af88c95503e209c7f6fbbc
Akeel Ather Medina <akeelmedina22@gmail.com>
2022-10-27 19:46:58 +0500

    update location

--- Kaavish/app/src/main/java/com/example/kaavish/MainActivity.kt
+++ Kaavish/app/src/main/java/com/example/kaavish/MainActivity.kt
@@ -1,26 +1,37 @@
 package com.example.kaavish
 
+import android.Manifest
 import android.content.Context
 import android.content.pm.ActivityInfo
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import android.widget.TextView
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var mSensorManager : SensorManager
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     private var mAccelerometer : Sensor ?= null
     private var mGyroscope : Sensor ?= null
     private var resume = false
+    // globally declare LocationRequest
+    private lateinit var locationRequest: LocationRequest
+
+    // globally declare LocationCallback
+    private lateinit var locationCallback: LocationCallback
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         window.setFlags(
@@ -31,16 +42,82 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         setContentView(R.layout.activity_main)
 
         mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
         mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+
+        getLocationUpdates()
+
     }
+
+    private fun getLocationUpdates()
+    {
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        locationRequest = LocationRequest()
+        locationRequest.interval = 50000
+        locationRequest.fastestInterval = 50000
+        locationRequest.smallestDisplacement = 170f // 170 m = 0.1 mile
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY //set according to your app function
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                println("hello")
+                locationResult ?: return
+
+                if (locationResult.locations.isNotEmpty()) {
+                    // get latest location
+                    val location =
+                        locationResult.lastLocation
+                    // use your location object
+                    // get latitude , longitude and other info from this
+                    val lat = location.latitude
+                    findViewById<TextView>(R.id.lat).text = lat.toString()
+                    val longt = location.longitude
+                    findViewById<TextView>(R.id.longt).text = longt.toString()
+
+                }
+            }
+        }
+    }
+
+    //start location updates
+    private fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            null /* Looper */
+        )
+    }
+
+    // stop location updates
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
         print("accuracy changed")
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
+        // update onlocation result here,, but how ?
         if (event != null && resume) {
             if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
                 val accX = event.values[0]
@@ -88,10 +165,12 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         super.onResume()
         mSensorManager.registerListener(this, mAccelerometer, 500000, 500000)
         mSensorManager.registerListener(this, mGyroscope, 500000, 500000)
+        startLocationUpdates()
     }
 
     override fun onPause() {
         super.onPause()
+        stopLocationUpdates()
         mSensorManager.unregisterListener(this)
     }
 
