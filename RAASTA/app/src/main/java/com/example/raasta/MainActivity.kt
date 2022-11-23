ef6d409118c894da44a10061feace3a0875489d1
Abeer Khan <54454858+abeerkhan99@users.noreply.github.com>
2022-11-23 06:16:06 +0500

    Update MainActivity.kt

--- RAASTA/app/src/main/java/com/example/raasta/MainActivity.kt
+++ RAASTA/app/src/main/java/com/example/raasta/MainActivity.kt
@@ -65,6 +65,10 @@ class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+       
+        // restrict the thread to work only in main thread
+        val policy = ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
 
         locationResult = Location("current_location")
         // layout initialization
@@ -95,7 +99,8 @@ class MainActivity : AppCompatActivity(){
 
         map = findViewById<MapView>(R.id.map)
         map.setTileSource(TileSourceFactory.MAPNIK)
-
+        map.isClickable = true
+        map.setMultiTouchControls(true)
 
         // get current location
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
@@ -104,31 +109,99 @@ class MainActivity : AppCompatActivity(){
         }
 
         println(locationResult.latitude)
-
+       
+        // hard-coded location point is used to center the map
         val mapController = map.controller
-        val startPoint = GeoPoint(locationResult.latitude, locationResult.longitude)
+        val startPoint = GeoPoint(24.956, 67.052)
         mapController.setZoom(18.5)
         mapController.setCenter(startPoint)
+        
+        // placing marker at the starting point which is the hard coded location
+        val startMarker = Marker(map)
+        startMarker.position = startPoint
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        map.overlays.add(startMarker)
 
+        // route generation
+        val roadManager: RoadManager = OSRMRoadManager(this, "Raasta")
 
+        val waypoints = ArrayList<GeoPoint>()
+        waypoints.add(startPoint)
+        // hard coding the end point location which is habib university
+        val endPoint = GeoPoint(24.905865, 67.138235)
+        waypoints.add(endPoint)
+
+        val road = roadManager.getRoad(waypoints)
+        // making the route
+        val roadOverlay: Polyline = RoadManager.buildRoadOverlay(road)
+        map.getOverlays().add(roadOverlay)
+
+        // add pin at habib univeristy
+        val endMarker = Marker(map)
+        endMarker.position = endPoint
+        endMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        map.overlays.add(endMarker)
+        
+        // drop a pin
+        val mReceive: MapEventsReceiver = object : MapEventsReceiver {
+            override fun singleTapConfirmedHelper(p: GeoPoint): Boolean {
+                return false
+            }
+
+            override fun longPressHelper(p: GeoPoint): Boolean {
+                // write your code here
+                // did not drop pin yet, just shows the location points rn
+                val str = p.latitude.toString() + " - " + p.longitude.toString()
+                Toast.makeText(baseContext, str ,Toast.LENGTH_LONG).show()
+                return false
+            }
+        }
+        val OverlayEvents = MapEventsOverlay(mReceive)
+        map.overlays.add(OverlayEvents)
+        
+        // adding marker at specific points of the route
+//        val nodeIcon = resources.getDrawable(R.drawable.marker_node)
+//        for (i in road.mNodes.indices) {
+//            val node = road.mNodes[i]
+//            val nodeMarker = Marker(map)
+//            nodeMarker.position = node.mLocation
+//            nodeMarker.icon = nodeIcon
+//            nodeMarker.title = "Step $i"
+//
+//            nodeMarker.setSnippet(node.mInstructions)
+//            nodeMarker.setSubDescription(Road.getLengthDurationText(this, node.mLength, node.mDuration))
+//            val icon = resources.getDrawable(R.drawable.ic_continue)
+//            nodeMarker.image = icon
+//            map.overlays.add(nodeMarker)
+//        }
+//
+//        // search for points of interests
+//        val poiProvider = NominatimPOIProvider("OSMBonusPackTutoUserAgent")
+//        val pois = poiProvider.getPOICloseTo(startPoint, "cinema", 50, 0.1)
+//
+//        val poiMarkers = FolderOverlay(this)
+//        map.overlays.add(poiMarkers)
+//
+//        val poiIcon = resources.getDrawable(R.drawable.marker_poi_default)
+//        for (poi in pois) {
+//            val poiMarker = Marker(map)
+//            poiMarker.title = poi.mType
+//            poiMarker.snippet = poi.mDescription
+//            poiMarker.position = poi.mLocation
+//            poiMarker.icon = poiIcon
+//            if (poi.mThumbnail != null) {
+//                poiMarker.setImage(BitmapDrawable(poi.mThumbnail))
+//            }
+//            poiMarkers.add(poiMarker)
+//        }
+
+   
 //        map.overlays.add(myLocationOverlay)
 
 //        map.invalidate()
 
         //Offline maps:
         //map.setUseDataConnection(true);
-
-        map.isClickable = true
-        map.setMultiTouchControls(true)
-
-//        val endPoint = GeoPoint(29.0, 70.0)
-//        waypoints.add(endPoint)
-//        val road : Road = roadManager.getRoad(waypoints)
-//        val roadOverlay: Polyline = RoadManager.buildRoadOverlay(road)
-//        map.getOverlays().add(roadOverlay)
-//        map.invalidate()
-
-
 
     }
 
