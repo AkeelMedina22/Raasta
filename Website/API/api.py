0a12b952bc960378013ba73cc0a07a5786e8f75f
Abeer Khan <54454858+abeerkhan99@users.noreply.github.com>
2023-02-23 22:16:52 +0500

    added automatic api documentation using flasgger

--- Website/API/api.py
+++ Website/API/api.py
@@ -3,6 +3,7 @@ from flask_restful import Api, Resource
 from firebase import firebase
 import uuid
 from flask_cors import CORS, cross_origin
+from flasgger import Swagger
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
@@ -10,8 +11,24 @@ key = ""
 # connect to firebase
 firebase = firebase.FirebaseApplication('https://raasta-c542d-default-rtdb.asia-southeast1.firebasedatabase.app/', None)
 
+Swagger(app)
+
 @app.route('/get_key', methods=['GET'])
 def api_key_request():
+    """
+    Generate API Key
+    ---
+    responses:
+      200:
+        description: API key generated. This key can be used to authenticate requests to the API and ensure that only authorized users are able to access sensitive data or perform certain actions.
+        schema:
+          properties:
+            key:
+              type: string
+              description: 128-bit random UUID
+      500:
+        description: Error generating API key
+    """
     # API key - make a token, send it to client
     uuid_str = str(uuid.uuid4())
     global key
@@ -21,6 +38,27 @@ def api_key_request():
 @app.route('/get_points', methods=['GET'])
 @cross_origin(origin='*')
 def get_potholes():
+    """
+    Get pothole points
+    ---
+    responses:
+      200:
+        description: Retrieve pothole points in the form of latitude and longitude coordinates from a Firebase Realtime Database and display them on a map as markers.
+        schema:
+          properties:
+            Pothole:
+              type: array
+              items:
+                type: array
+                items:
+                  type: number
+                  description: latitude and longitude co-ordinates
+          example: [{"Pothole": [[24.9597674, 67.0627178], [24.9597674, 67.0627178]]}]
+      401:
+        description: Invalid API key
+      500:
+        description: Error retrieving pothole points
+    """
     global key
     # get all pothole points from database and send to client
     if request.headers["Authorization"] == key:
