99d20c9ececdb6e9ae085ea2e68612d75134d27a
zoha99 <60128328+zoha99@users.noreply.github.com>
2023-05-11 09:57:40 +0500

    Latest cleaned code

--- Website/Flutter Website/lib/classes/api.dart
+++ Website/Flutter Website/lib/classes/api.dart
@@ -3,28 +3,12 @@ import 'dart:convert';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 
-//String APIURL = "127.0.0.1:5000";
 String APIURL = "raasta.pythonanywhere.com";
 
 class API {
   static Future<String> getKey() async {
-    // var headers = {'Authorization': 'b08049fe-6f37-470a-9e10-9644fb7a5cb1'};
-    // var request = http.Request(
-    //   'GET',
-    //   Uri.parse('https://raasta.pythonanywhere.com/get_key'),
-    // );
-    // // request.headers.addAll(headers);
-
-    // http.StreamedResponse response = await request.send();
-
-    // if (response.statusCode == 200) {
-    //   return jsonDecode(await response.stream.bytesToString())['key'];
-    // } else {
-    //   throw Exception(response.reasonPhrase);
-    // }
 
     http.Response response = await http.get(Uri.https(APIURL, '/get_key'));
-    //http.Response response =        await http.get(Uri.http('https://raasta.pythonanywhere.com/get_key'));
     var data = jsonDecode(response.body);
     var message = data["key"];
     final api_key = message;
@@ -33,49 +17,8 @@ class API {
   }
 
   static Future<List<Map>> getPoints(String key, String type_point) async {
-    //   var headers = {'Authorization': key};
-    //   var request = http.Request(
-    //     'GET',
-    //     Uri.parse('https://raasta.pythonanywhere.com/get_points'),
-    //   );
-    //   request.headers.addAll(headers);
-
-    //   http.StreamedResponse response = await request.send();
-
-    //   if (response.statusCode == 200) {
-    //     var x = jsonDecode(await response.stream.bytesToString());
-    //     var potholes = x['potholes']
-    //         .map(
-    //           (e) => {
-    //             'type': 'pothole',
-    //             'latLng': LatLng(e[0], e[1]),
-    //           },
-    //         )
-    //         .toList();
-    //     var badroads = x['badroads']
-    //         .map(
-    //           (e) => {
-    //             'type': 'badroad',
-    //             'latLng': LatLng(e[0], e[1]),
-    //           },
-    //         )
-    //         .toList();
-    //     var speedbreakers = x['speedbreakers']
-    //         .map(
-    //           (e) => {
-    //             'type': 'speedbreaker',
-    //             'latLng': LatLng(e[0], e[1]),
-    //           },
-    //         )
-    //         .toList();
-    //     return [...potholes, ...badroads, ...speedbreakers];
-    //   } else {
-    //     throw Exception(response.reasonPhrase);
-    //   }
-
     http.Response data = await http.get(
         Uri.https(APIURL, '/get_points/$type_point'),
-        //Uri.http('https://raasta.pythonanywhere.com/get_points/$type_point'),
         headers: {"Authorization": key});
     var data_points = jsonDecode(data.body);
     final points = data_points["Points"];
@@ -96,7 +39,6 @@ class API {
   static Future<LatLng> getPlacePoints(place) async {
     http.Response response =
         await http.get(Uri.https(APIURL, '/get_place_coords/$place'));
-        //await http.get(Uri.http('https://raasta.pythonanywhere.com/get_place_coords/$place'));
     final place_info = jsonDecode(response.body);
     final place_lat = place_info['result']['geometry']['location']['lat'];
     final place_lng = place_info['result']['geometry']['location']['lng'];
@@ -106,7 +48,6 @@ class API {
   static Future<List> getSuggestions(searchTerm, c_lat, c_lng) async {
     http.Response response = await http
         .get(Uri.https(APIURL, '/autocomplete/$searchTerm/$c_lat/$c_lng'));
-        //.get(Uri.http(            'https://raasta.pythonanywhere.com/autocomplete/$searchTerm/$c_lat/$c_lng'));
     List predictions = jsonDecode(response.body);
     return predictions;
   }
@@ -114,8 +55,6 @@ class API {
   static Future<dynamic> getDirections(o_lat, o_lng, d_lat, d_lng) async {
     http.Response route = await http
         .get(Uri.https(APIURL, '/directions/$o_lat/$o_lng/$d_lat/$d_lng'));
-        //.get(Uri.http(            'https://raasta.pythonanywhere.com//directions/$o_lat/$o_lng/$d_lat/$d_lng'));
-
     var data = jsonDecode(route.body);
     return data;
   }
